b4cdd0df49993fb42bff8158f96e10042434cad9
Matthias Noback <matthiasnoback@gmail.com>
2017-04-03 15:34:30 +0200

    Revert some of the changes for the workshop

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ header('Content-Type: text/plain', true, 200);
 echo "Hello World!\n";
 
 // Uncomment the following lines when you're at assignment 07:
-
-$redis = new Redis();
-$redis->connect('redis');
-echo 'Number of visits: ' . $redis->incr('visitors') . "\n";
+//
+//$redis = new Redis();
+//$redis->connect('redis');
+//echo 'Number of visits: ' . $redis->incr('visitors') . "\n";
